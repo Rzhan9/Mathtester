57cafad353a1e90fff588730a2163eb3edeae462
Rzhan9 <rzhangjs@gmail.com>
2026-07-12 20:28:35 -0400

    num type checker change

--- division.py
+++ division.py
@@ -1,5 +1,5 @@
 def div(n1, n2):
-    if type(n1) and type(n2) not in [int, float]:
+    if type(n1) not in [int, float] or type(n2) not in [int, float]:
         raise TypeError("Inputs must be numbers!")
     if n2 == 0:
         raise ZeroDivisionError("Divisor cannot be zero!")
