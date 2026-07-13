8295dbd56e6fe62c51491a952ec0f75750be2052
Rzhan9 <rzhangjs@gmail.com>
2026-07-13 16:04:06 -0400

    added exponent

--- exponent.py
+++ exponent.py
@@ -1,4 +1,4 @@
-def exponent(n1, n2):
+def exp(n1, n2):
     if type(n1) not in [int, float] or type(n2) not in [int, float]:
         raise TypeError("Inputs must be numbers!")
     return n1 ** n2
