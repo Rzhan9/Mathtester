cddfd2773d243d5edc1eb36ac46fc15cdd3f36de
Rzhan9 <rzhangjs@gmail.com>
2026-07-13 16:07:58 -0400

    changed typeerror message

--- exponent.py
+++ exponent.py
@@ -1,4 +1,4 @@
 def exp(n1, n2):
     if type(n1) not in [int, float] or type(n2) not in [int, float]:
-        raise TypeError("Inputs must be numbers!")
+        raise TypeError("Inputs must be numbers! MAKE THE INPUTS NUMBERS! NUMBERS I TELL YOU!")
     return n1 ** n2
