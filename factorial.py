627ba04c35065dcf232a5707591aa5884faf3cca
Rzhan9 <rzhangjs@gmail.com>
2026-07-27 16:12:39 -0400

    negative num factorials check

--- factorial.py
+++ factorial.py
@@ -3,4 +3,6 @@ from math import factorial as fac
 def factorial(n: int):
     if type(n) is not int:
         raise ValueError("Inputs must be integers!")
+    if n < 0:
+        raise ValueError("Inputs must be non-negative!")
     return fac(n)
